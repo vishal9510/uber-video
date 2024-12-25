62965ea49085a789e2a7a205b2e3092af3df4885
vishal <vishalsavaliya2912@gmail.com>
2024-12-26 03:08:02 +0530

    backend code intigrate

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -32,7 +32,7 @@ const UserSignup = () => {
       password: password
     }
 
-    const response = await axios.post(`${'http://localhost:5173'}/users/register`, newUser)
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/register`, newUser)
 
     if (response.status === 201) {
       const data = response.data
